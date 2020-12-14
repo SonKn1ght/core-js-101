aab9085c842b4cc79be015ce456778ef3a707d1f
SonKn1ght <maximiilian321@gmail.com>
2020-12-14 23:21:59 +0300

    feat: 99 passing

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -112,8 +112,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let att = attempts;
+
+  return () => {
+    while (att) {
+      try {
+        return func();
+      } catch (e) {
+        att -= 1;
+      }
+    }
+    return null;
+  };
 }
 
 
@@ -140,8 +151,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...arg) => {
+    const argLog = JSON.stringify(arg).slice(1, -1);
+    logFunc(`${func.name}(${argLog}) starts`);
+    const result = func(...arg);
+    logFunc(`${func.name}(${argLog}) ends`);
+    return result;
+  };
 }
 
 
@@ -158,8 +175,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...arg) => fn(...args1, ...arg);
 }
 
 
@@ -180,8 +197,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  return () => {
+    count += 1;
+    return count - 1;
+  };
 }
 
 
