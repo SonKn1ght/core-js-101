5bd76249b3a37df675b7c40758010a7e361c2c24
SonKn1ght <maximiilian321@gmail.com>
2020-12-14 03:01:15 +0300

    feat: 94 passing

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 
@@ -62,8 +62,8 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...arg) {
+  return (x) => arg.reduce((acc, cur, i) => acc + (cur * (x ** (arg.length - 1 - i))), 0);
 }
 
 
@@ -81,8 +81,19 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+
+  return (...arg) => {
+    const keyCacheItem = JSON.stringify(arg);
+
+    if (keyCacheItem in cache) {
+      return cache[keyCacheItem];
+    }
+    const result = func.call(this, ...arg);
+    cache[keyCacheItem] = result;
+    return result;
+  };
 }
 
 
